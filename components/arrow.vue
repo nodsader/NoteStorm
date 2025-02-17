dc4339fbf2edd14b96e0418d4efb382bd586c927
Nodsader <nodsader@gmail.com>
2025-02-17 16:17:41 +0100

    Initial_commit

---
+++ components/arrow.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg width="15px" height="15px" viewBox="0 0 160 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(1,0,0,1,-454.873,-370.841)">
+        <g transform="matrix(0.29386,0,0,1,381.846,36)">
+            <path d="M248.51,363.051L248.51,355.789L731.185,355.789L709.343,334.841L792.987,359.42L709.343,384L731.185,363.051L248.51,363.051Z"/>
+        </g>
+    </g>
+</svg>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="flex bg-black h-screen">
+   <!--sidebar-->
+   <div class="bg-zinc-900 w-md p-8">
+     <Logo/>
+      <h1 class="text-white text-lg font-bold mt-8">Sign up for a free account</h1>
+      <p class="text-txt-primary text-sm mt-0.5">Already registered ? <span class="font-bold text-yellow underline">Log in</span> to your account</p>
+     <div class="mt-8">
+       <label for="" class="text-white text-sm mb-0.5">Email</label>
+       <input type="email" placeholder="you@example.com" class="bg-input block border border-border rounded text-white px-4 py-1.5 w-full placeholder:text-zinc-500 text-sm"/>
+     </div>
+
+     <div class="mt-6">
+       <label for="" class="text-white text-sm mb-0.5">Password</label>
+       <input type="password" placeholder="*******" class="bg-input block border border-border rounded text-white px-4 py-1.5 w-full placeholder:text-zinc-500 text-sm"/>
+     </div>
+
+<!--     sign up button -->
+     <div>
+       <button class="w-full mt-4 bg-yellow rounded-full py-2 text-sm font-bold flex justify-center items-center space-x-2"><span>Sign Up</span>
+       <arrow/>
+       </button>
+     </div>
+<!--     /signup-->
+   </div>
+
+   <!-- /sidebar -->
+   <!-- Note introduction  -->
+   <div></div>
+   <!-- /note intro   -->
+ </div>
+</template>
+
+<style scoped>
+
+</style>
