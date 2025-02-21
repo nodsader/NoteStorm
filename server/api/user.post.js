462a4f36e438ce6f0900c51a9cf235a15e8835e2
Nodsader <nodsader@gmail.com>
2025-02-21 14:50:57 +0100

    Début de la phase de hash du mot de passe utilisateur une fois stocké dans la db

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,5 +1,7 @@
 // /api/user POST
 
+//Salts, pour la sécurité, il est évident qu'il ne vaut mieux pas envoyer le mot de passe, de façon claire à la DB. C'est pourquoi on va utiliser la technique du 'salt'.
+
 import prisma from "~/lib/prisma";
 
 
